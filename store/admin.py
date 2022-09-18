4bf5c1318b655606015269f08f5c077982e6bf92
anluke <retzul@outlook.com>
2022-09-18 16:51:30 +0000

    added admin pane to customer, category and product section

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,39 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
 
-from .models import *
 
-admin.site.register(Customer)
-admin.site.register(Category)
-admin.site.register(Product)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'name',
+        'email',
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'digital',
+        'image',
+    )
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)

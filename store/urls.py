179e7ef67c02b8bb20ae82b6f14574609e456063
anluke <retzul@outlook.com>
2022-09-18 20:56:59 +0000

    created a product info page with, added a breadcrumbs navigation and added content to my store

---
+++ store/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('<product_id>', views.product_info, name='product_info'),
+]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 # Create your views here.
+
+
+def store(request):
+    """ A view to show our Store page, including sorting and search queries """
+
+    store = Product.objects.all()
+
+
+    context = {
+        'store': store,
+    }
+
+    return render(request, 'store/store.html', context)
+
+
+
+def product_info(request, product_id):
+    """ A view to show product information """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'store/product_info.html', context)

0375f60e4140a05cd36cce3196f574b20cd05e99
anluke <retzul@outlook.com>
2022-10-02 14:24:27 +0000

    added functionality to page search bar and queries

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import *
 
 # Create your views here.
@@ -8,10 +10,21 @@ def store(request):
     """ A view to show our Store page, including sorting and search queries """
 
     store = Product.objects.all()
+    query = None
 
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('store'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            store = store.filter(queries)
 
     context = {
         'store': store,
+        'search-term': query,
     }
 
     return render(request, 'store/store.html', context)

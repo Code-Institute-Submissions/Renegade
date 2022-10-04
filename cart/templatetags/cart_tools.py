adaa41d7d3b6a559734065d85caa920643912a25
anluke <retzul@outlook.com>
2022-10-04 22:17:24 +0000

    created custom template tag for calculating subtotal and added options to remove and update cart items

---
+++ cart/templatetags/cart_tools.py
@@ -0,0 +1,9 @@
+from django import template
+
+# Customer template tag for calculating subtotal of every item when quantity is updated
+
+register = template.Library()
+
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    return price * quantity

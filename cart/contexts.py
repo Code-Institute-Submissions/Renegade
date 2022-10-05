10657116a85782eecb007ea1331163acd332cec2
anluke <retzul@outlook.com>
2022-10-05 22:53:08 +0000

    created a separate sample label for free shipping and updated cart responsiveness when certain amount is met

--- cart/contexts.py
+++ cart/contexts.py
@@ -33,15 +33,27 @@ def cart_contents(request):
                     'size': size,
                 })
     
-    delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+
     grand_total = delivery + total
+    sample_delivery_data = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+
     
     context = {
         'cart_items': cart_items,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
+        'sample_delivery_data': sample_delivery_data
     }
 
     return context

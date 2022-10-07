ebe3de89d6853d8c5ec54972f6757cdeb031fb86
anluke <retzul@outlook.com>
2022-10-07 14:05:17 +0000

    added filter by today's date to my tour app to hide innactive events

--- home/views.py
+++ home/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from tour.models import Tour
 from songs.models import Song
+from datetime import date
 
 # Create your views here.
 
 
 def index(request):
-    tour = Tour.objects.all()
+    tour = Tour.objects.filter(date__gte=date.today()).order_by('date')
     song = Song.objects.all()
     return render(request, 'home/index.html', {"tour": tour, "song": song})
 
--- tour/views.py
+++ tour/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from .models import Tour
-from datetime import datetime
+from datetime import date
 
 # Create your views here.
 
 
 def tour_date(request):
     """ A view to show our Tour page """
-    tour = Tour.objects.all().order_by('date')  
+    tour = Tour.objects.filter(date__gte=date.today()).order_by('date')
     
     context = {
         'tour': tour,

3ae95eba024a37a3c758c7136b0b9ba69584674c
anluke <retzul@outlook.com>
2022-10-17 23:08:36 +0000

    added 'accounts app' admin view to admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'default_phone_number',
+        'default_town_or_city',
+        'default_postcode',
+        'default_country',
+    )
+
+
+
+admin.site.register(UserAccount, UserAccountAdmin)

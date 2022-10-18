03ce989c4d26c3f4cf01b9a75096162871ff4675
anluke <retzul@outlook.com>
2022-10-18 09:38:14 +0000

    deleted old sqlite db and imported json files manually

--- home/models.py
+++ home/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 
-class Song(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False)
-    date_added = models.DateTimeField(auto_now_add=True)
-    url = models.URLField(max_length=250)
-
-    def __str__(self):
-        return self.name
-

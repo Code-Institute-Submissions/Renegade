8d4a9051e2b2900e7bc7a1c2aa332952c5b84814
anluke <retzul@outlook.com>
2022-10-19 16:09:39 +0000

    added email config vars to heroku app and connected to django to send emails

--- renegade/settings.py
+++ renegade/settings.py
@@ -124,9 +124,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-#required for gitpod to store the recordings in the session for testing
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 
 # Additional Authentication settings
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
@@ -244,4 +241,15 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'renegadeband@example.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'renegadeband@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
